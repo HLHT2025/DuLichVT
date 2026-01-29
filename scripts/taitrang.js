afa6b563d2d007c538bce66b74dcab9d09ff1a0e
HLHT2025 <hoshiyomiiam@gmail.com>
2026-01-29 21:46:49 +0700

    Delete scripts/taitrang.js

--- scripts/taitrang.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(e){
-  e.preventDefault();
-  alert("Cảm ơn bạn! Chúng tôi sẽ liên hệ tư vấn trong thời gian sớm nhất.");
-});
-// JavaScript Document
